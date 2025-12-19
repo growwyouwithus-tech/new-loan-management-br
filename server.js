4194c2ad3a0b11ec132028d5beb14ec838ccd730
Amit Bhardwaj <growwyouwithus@gmail.com>
2025-12-19 16:04:41 +0530

    database connect2

--- server.js
+++ server.js
@@ -37,8 +37,8 @@ app.use(cors({
 }));
 app.use(compression());
 app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use('/api/', limiter);
 
 app.use('/uploads', express.static('uploads'));
