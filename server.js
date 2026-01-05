0787d83cbdba4c968508ad2837ce2233209468fb
Amit Bhardwaj <growwyouwithus@gmail.com>
2026-01-05 17:13:28 +0530

    update 1

--- server.js
+++ server.js
@@ -72,8 +72,10 @@ app.use('/api/users', userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
-});
+if (process.env.VERCEL !== '1') {
+  app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
+  });
+}
 
 export default app;
