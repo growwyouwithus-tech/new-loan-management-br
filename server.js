346ef7bf4c5933c141e09eb01c56b49c02e44632
Amit Bhardwaj <growwyouwithus@gmail.com>
2026-01-12 14:02:07 +0530

    loan update

--- server.js
+++ server.js
@@ -35,7 +35,14 @@ app.use(helmet({
   crossOriginResourcePolicy: { policy: "cross-origin" }
 }));
 app.use(cors({
-  origin: process.env.CORS_ORIGIN?.split(',') || ['http://localhost:5173', 'http://localhost:5174'],
+  origin: process.env.CORS_ORIGIN?.split(',') || [
+    'http://localhost:5173',
+    'https://localhost:5173',
+    'http://localhost:5174',
+    'https://localhost:5174',
+    'http://localhost:5000',
+    'https://localhost:5000'
+  ],
   credentials: true,
 }));
 app.use(compression());
